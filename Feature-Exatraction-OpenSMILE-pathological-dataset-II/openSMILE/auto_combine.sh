74da65c4d56947b15c35138bf4abb7ab840ffee3
Aashna Jena <32356795+aashnajena@users.noreply.github.com>
2020-11-14 00:04:09 +0530

    Update auto_combine.sh

--- Feature-Exatraction-OpenSMILE-pathological-dataset-II/openSMILE/auto_combine.sh
+++ Feature-Exatraction-OpenSMILE-pathological-dataset-II/openSMILE/auto_combine.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 input="./patient_wav_files.txt"
+# add input file name here
+
 counter=1
 while IFS= read -r line
 do
   if [ $counter -eq 1 ]
   then
-      SMILExtract -C ../config/ComParE_2016.conf -I $line -csvoutput ./outputs/patient_output.csv -appendcsv 1
+      SMILExtract -C ../config/ComParE_2016.conf -I $line -csvoutput ./outputs/patient_output.csv -appendcsv 1 -headercsv 1
   else 
-      SMILExtract -C ../config/ComParE_2016.conf -I $line -csvoutput ./outputs/patient_output.csv -appendcsv 1
+      SMILExtract -C ../config/ComParE_2016.conf -I $line -csvoutput ./outputs/patient_output.csv -appendcsv 1 -headercsv 0
   fi
 done < "$input"
